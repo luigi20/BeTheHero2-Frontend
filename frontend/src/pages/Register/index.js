1fb736ee6d9715365d0ed098a727255d70759829
Luis <luisopentec@gmail.com>
2021-12-15 17:56:20 -0300

    teste

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -86,6 +86,10 @@ export default function Register() {
                     <input placeholder="Whatsapp"
                         {...register('whatsapp', { required: true })}
                     />
+                    <select id="uf">
+                        <option value="0">UF</option>
+
+                    </select>
                     <div className="input-group">
                         <select id="uf" style={{ width: 90 }}
                             value={selectedUf}
