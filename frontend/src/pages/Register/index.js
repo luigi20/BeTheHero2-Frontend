82b50ac68a0a300fe39a48e86abd294877736382
Luis <luisopentec@gmail.com>
2021-12-15 17:43:16 -0300

    changes in register form

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import './styles.css';
 import { Link, useNavigate } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 import { useForm } from 'react-hook-form';
-
+import axios from 'axios';
 export default function Register() {
-
+    const [selectedUf, setSelectedUf] = useState('0');
+    const [citiesNames, setCitiesNames] = useState([]);
+    const [ufs, setUfs] = useState([]);
+    const [selectedCity, setSelectedCity] = useState('0');
     const navigate = useNavigate();
     const { register, handleSubmit } = useForm({
         defaultValues: { name: "", email: "", password: "", uf: "", whatsapp: "", city: "" },
     });
+    useEffect(() => {
+        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then(response => {
+            const ufInitials = response.data.map(uf => uf.sigla);
+            setUfs(ufInitials);
+        });
+    }, []);
+    useEffect(() => {
+        if (selectedUf === '0') {
+            return;
+        }
+        axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+            const cityNames = response.data.map(city => city.nome);
+            setCitiesNames(cityNames);
+        });
+    }, [selectedUf]);
+    function handleSelectUf(event) {
+        const uf = event.target.value;
+        setSelectedUf(uf);
+    }
+    function handleSelectCity(event) {
+        const city = event.target.value;
+        setSelectedCity(city);
+    }
 
     async function handleRegister({ name, email, password, uf, whatsapp, city }, e) {
         e.preventDefault();
@@ -23,7 +49,9 @@ export default function Register() {
             city,
             uf
         };
+
         try {
+            console.log(dataOng);
             await api.post('/register_ongs', dataOng);
             alert("Cadastro Realizado Com Sucesso.");
             navigate('/');
@@ -59,8 +87,26 @@ export default function Register() {
                         {...register('whatsapp', { required: true })}
                     />
                     <div className="input-group">
-                        <input placeholder="Cidade"  {...register('city', { required: true })} />
-                        <input placeholder="UF" style={{ width: 80 }} {...register('uf', { required: true })} />
+                        <select id="uf" style={{ width: 90 }}
+                            value={selectedUf}
+                            {...register('uf', { required: true })}
+                            onChange={handleSelectUf}>
+                            <option value="0">UF</option>
+                            {ufs.map(uf => (
+                                <option key={uf} value={uf}>{uf}</option>
+                            ))}
+                        </select>
+                        <select
+                            id="city"
+                            {...register('city', { required: true })}
+                            onChange={handleSelectCity} >
+                            <option value="0">Selecione uma Cidade</option>
+                            {
+                                citiesNames.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
+                        </select>
+
                     </div>
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
